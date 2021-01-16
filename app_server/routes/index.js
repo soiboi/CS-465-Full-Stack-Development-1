ebef3e46d0b0ff1471152a6001041558bb164311
Sawyer <sawyerhannel94@gmail.com>
2021-01-16 10:19:25 -0800

    use mvc pattern to manage application logic

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const control = require('../controllers/main')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', control.index); 
 
 module.exports = router;
